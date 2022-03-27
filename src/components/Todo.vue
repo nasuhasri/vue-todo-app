6b42241eddfb635a154e37d64c9fbdbacd4d1b1a
Nasuha Asri <nasuhasri00@gmail.com>
2022-03-27 22:08:11 +0800

    marking todo as completed

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,7 +1,9 @@
 <!--- for every todo in our list the “Todo” component is used with it’s own unique data -->
 <template>
-  <div>
-    <p>{{ todo.title }}</p>
+  <!-- only use the class if todo is completed -->
+  <div v-bind:class="{ 'completed': todo.completed }">
+    <!-- change the todo status -->
+    <p v-on:click="markComplete">{{ todo.title }}</p>
   </div>
 </template>
 
@@ -10,10 +12,17 @@ export default {
   name: 'Todo',
   props: [
     "todo"
-  ]
+  ],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .completed {
+    text-decoration: line-through;
+  }
 </style>
